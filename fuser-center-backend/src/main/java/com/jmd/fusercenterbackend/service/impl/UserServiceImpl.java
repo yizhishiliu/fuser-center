d2b769321fb15937654dc30dfeb45f555d976d03
JiangMingda <15519688306@163.com>
2024-05-06 01:35:30 +0800

    后端项目初始化+注册登录简单实现

---
+++ fuser-center-backend/src/main/java/com/jmd/fusercenterbackend/service/impl/UserServiceImpl.java
@@ -0,0 +1,140 @@
+package com.jmd.fusercenterbackend.service.impl;
+import java.util.Date;
+
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.jmd.fusercenterbackend.model.domain.User;
+import com.jmd.fusercenterbackend.service.UserService;
+import com.jmd.fusercenterbackend.mapper.UserMapper;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.stereotype.Service;
+import org.springframework.util.DigestUtils;
+
+import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+import java.security.NoSuchAlgorithmException;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+* @author ada
+* @description 针对表【user(用户)】的数据库操作Service实现
+* @createDate 2024-05-04 16:40:34
+*/
+@Service
+@Slf4j
+public class UserServiceImpl extends ServiceImpl<UserMapper, User> implements UserService{
+
+    @Resource
+    private UserMapper userMapper;
+
+    /**
+     * 盐值
+     */
+    private static final String SALT = "ada";
+
+    private static final String USER_LOGIN_STATE = "userLoginState";
+
+    @Override
+    public long userRegister(String userAccount, String userPassword, String checkPassword) {
+        // 1. 校验
+        if (StringUtils.isAnyBlank(userAccount, userPassword, checkPassword)) {
+            return -1;
+        }
+        if (userAccount.length() < 4) {
+            return -1;
+        }
+        if (userPassword.length() < 6 || checkPassword.length() < 6) {
+            return -1;
+        }
+        // 账户不能包含特殊字符
+        // 字符串中只包含字母和数字
+        String validPattern = "^[a-zA-Z0-9]+$";
+        Matcher matcher = Pattern.compile(validPattern).matcher(userAccount);
+        // !matcher.find()
+        if (!matcher.matches()) {
+            return -1;
+        }
+        if (!userPassword.equals(checkPassword)) {
+            return -1;
+        }
+        // 账户不能重复（这里查询数据库，应该放到后面，提升性能）
+        QueryWrapper<User> userQueryWrapper = new QueryWrapper<>();
+        userQueryWrapper.eq("userAccount", userAccount);
+        long count = userMapper.selectCount(userQueryWrapper);
+        if (count > 0) {
+            return -1;
+        }
+
+        // 2. 加密
+        String encryptPassword = DigestUtils.md5DigestAsHex((SALT + userPassword).getBytes());
+
+        // 3. 插入数据
+        User user = new User();
+        user.setUserAccount(userAccount);
+        user.setUserPassword(encryptPassword);
+        boolean saveResult = this.save(user);
+        if (!saveResult) {
+            return -1;
+        }
+
+        return user.getId();
+    }
+
+    @Override
+    public User userLogin(String userAccount, String userPassword, HttpServletRequest request) {
+        // 1. 校验
+        if (StringUtils.isAnyBlank(userAccount, userPassword)) {
+            return null;
+        }
+        if (userAccount.length() < 4) {
+            return null;
+        }
+        if (userPassword.length() < 6) {
+            return null;
+        }
+        // 账户不能包含特殊字符
+        // 字符串中只包含字母和数字
+        String validPattern = "^[a-zA-Z0-9]+$";
+        Matcher matcher = Pattern.compile(validPattern).matcher(userAccount);
+        // !matcher.find()
+        if (!matcher.matches()) {
+            return null;
+        }
+
+        // 2. 加密
+        String encryptPassword = DigestUtils.md5DigestAsHex((SALT + userPassword).getBytes());
+
+        // 查询用户是否存在
+        QueryWrapper<User> userQueryWrapper = new QueryWrapper<>();
+        userQueryWrapper.eq("userAccount", userAccount);
+        userQueryWrapper.eq("userPassword", encryptPassword);
+        User user = userMapper.selectOne(userQueryWrapper);
+        // 用户不存在
+        if (user == null) {
+            log.info("user login failed, userAccount cannot match userPassword");
+            return null;
+        }
+
+        // 3. 用户脱敏
+        User safetyUser = new User();
+        safetyUser.setId(user.getId());
+        safetyUser.setUsername(user.getUsername());
+        safetyUser.setUserAccount(user.getUserAccount());
+        safetyUser.setAvatarUrl(user.getAvatarUrl());
+        safetyUser.setGender(user.getGender());
+        safetyUser.setPhone(user.getPhone());
+        safetyUser.setEmail(user.getEmail());
+        safetyUser.setUserStatus(0);
+        safetyUser.setCreateTime(user.getCreateTime());
+        safetyUser.setUpdateTime(user.getUpdateTime());
+        safetyUser.setUserRole(user.getUserRole());
+        safetyUser.setPlanetCode(user.getPlanetCode());
+
+        // 4. 记录用户登录态
+        request.getSession().setAttribute(USER_LOGIN_STATE, user);
+
+        return user;
+    }
+}
