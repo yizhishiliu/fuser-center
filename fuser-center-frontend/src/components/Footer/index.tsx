ac4976cfa398fdf84cc7d10e8ff04c68e5e379f9
JiangMingda <15519688306@163.com>
2024-05-08 23:58:53 +0800

    登录页简单修改

--- fuser-center-frontend/src/components/Footer/index.tsx
+++ fuser-center-frontend/src/components/Footer/index.tsx
@@ -3,28 +3,31 @@ import { DefaultFooter } from '@ant-design/pro-components';
 import React from 'react';
 
 const Footer: React.FC = () => {
+  const defaultMessage = '大狐狸出品';
+  const currentYear = new Date().getFullYear();
   return (
     <DefaultFooter
       style={{
         background: 'none',
       }}
+      copyright={`${currentYear} ${defaultMessage}`}
       links={[
         {
-          key: 'Ant Design Pro',
-          title: 'Ant Design Pro',
-          href: 'https://pro.ant.design',
+          key: 'user center',
+          title: 'fuser-center',
+          href: 'https://github.com/Mingdaj/user-center/',
+          blankTarget: true,
+        },
+        {
+          key: 'csdn',
+          title: '我的博客',
+          href: 'https://blog.csdn.net/weixin_52164430?spm=1000.2115.3001.5343',
           blankTarget: true,
         },
         {
           key: 'github',
-          title: <GithubOutlined />,
-          href: 'https://github.com/ant-design/ant-design-pro',
-          blankTarget: true,
-        },
-        {
-          key: 'Ant Design',
-          title: 'Ant Design',
-          href: 'https://ant.design',
+          title: <><GithubOutlined />大狐狸 Github</>,
+          href: 'https://github.com/Mingdaj',
           blankTarget: true,
         },
       ]}
--- fuser-center-frontend/src/pages/User/Login/index.tsx
+++ fuser-center-frontend/src/pages/User/Login/index.tsx
@@ -21,6 +21,7 @@ import { createStyles } from 'antd-style';
 import React, { useState } from 'react';
 import { flushSync } from 'react-dom';
 import Settings from '../../../../config/defaultSettings';
+import {BLOG_LINK, SYSTEM_LOGO} from '@/constants';
 const useStyles = createStyles(({ token }) => {
   return {
     action: {
@@ -144,15 +145,11 @@ const Login: React.FC = () => {
             minWidth: 280,
             maxWidth: '75vw',
           }}
-          logo={<img alt="logo" src="/logo.svg" />}
-          title="Ant Design"
-          subTitle={'Ant Design 是西湖区最具影响力的 Web 设计规范'}
+          logo={<img alt="logo" src={SYSTEM_LOGO}/>}
+          title="大狐狸 SER-CENTER"
+          subTitle={<a href={BLOG_LINK} target="_blank" rel="noreferrer"> 有友携行，理想长鸣！ </a>}
           initialValues={{
             autoLogin: true,
-          }}
-          actions={['其他登录方式 :', <ActionIcons key="icons" />]}
-          onFinish={async (values) => {
-            await handleSubmit(values as API.LoginParams);
           }}
         >
           <Tabs
@@ -164,15 +161,11 @@ const Login: React.FC = () => {
                 key: 'account',
                 label: '账户密码登录',
               },
-              {
-                key: 'mobile',
-                label: '手机号登录',
-              },
             ]}
           />
 
           {status === 'error' && loginType === 'account' && (
-            <LoginMessage content={'错误的用户名和密码(admin/ant.design)'} />
+            <LoginMessage content={'错误的用户名和密码'} />
           )}
           {type === 'account' && (
             <>
@@ -182,7 +175,7 @@ const Login: React.FC = () => {
                   size: 'large',
                   prefix: <UserOutlined />,
                 }}
-                placeholder={'用户名: admin or user'}
+                placeholder='请输入用户名: '
                 rules={[
                   {
                     required: true,
@@ -196,7 +189,7 @@ const Login: React.FC = () => {
                   size: 'large',
                   prefix: <LockOutlined />,
                 }}
-                placeholder={'密码: ant.design'}
+                placeholder='请输入密码: '
                 rules={[
                   {
                     required: true,
@@ -208,60 +201,6 @@ const Login: React.FC = () => {
           )}
 
           {status === 'error' && loginType === 'mobile' && <LoginMessage content="验证码错误" />}
-          {type === 'mobile' && (
-            <>
-              <ProFormText
-                fieldProps={{
-                  size: 'large',
-                  prefix: <MobileOutlined />,
-                }}
-                name="mobile"
-                placeholder={'请输入手机号！'}
-                rules={[
-                  {
-                    required: true,
-                    message: '手机号是必填项！',
-                  },
-                  {
-                    pattern: /^1\d{10}$/,
-                    message: '不合法的手机号！',
-                  },
-                ]}
-              />
-              <ProFormCaptcha
-                fieldProps={{
-                  size: 'large',
-                  prefix: <LockOutlined />,
-                }}
-                captchaProps={{
-                  size: 'large',
-                }}
-                placeholder={'请输入验证码！'}
-                captchaTextRender={(timing, count) => {
-                  if (timing) {
-                    return `${count} ${'秒后重新获取'}`;
-                  }
-                  return '获取验证码';
-                }}
-                name="captcha"
-                rules={[
-                  {
-                    required: true,
-                    message: '验证码是必填项！',
-                  },
-                ]}
-                onGetCaptcha={async (phone) => {
-                  const result = await getFakeCaptcha({
-                    phone,
-                  });
-                  if (!result) {
-                    return;
-                  }
-                  message.success('获取验证码成功！验证码为：1234');
-                }}
-              />
-            </>
-          )}
           <div
             style={{
               marginBottom: 24,
@@ -274,8 +213,10 @@ const Login: React.FC = () => {
               style={{
                 float: 'right',
               }}
+              href={BLOG_LINK}
+              target="_blank"
             >
-              忘记密码 ?
+              忘记密码 ? 请联系大狐狸
             </a>
           </div>
         </LoginForm>
