0cb9999f99299d9f4d9223a47d0c733a6d39dac9
JiangMingda <15519688306@163.com>
2024-07-05 00:22:24 +0800

    封装前端全局请求响应拦截器

--- fuser-center-frontend/src/pages/User/Register/index.tsx
+++ fuser-center-frontend/src/pages/User/Register/index.tsx
@@ -21,8 +21,8 @@ const Register: React.FC = () => {
 
     try {
       // 注册
-      const res = await register(values);
-      if (res.code === 0 && res.data > 0) {
+      const id = await register(values);
+      if (id) {
         const defaultLoginSuccessMessage = '注册成功！';
         message.success(defaultLoginSuccessMessage);
         /** 此方法会跳转到 redirect 参数所在的位置 */
@@ -33,12 +33,10 @@ const Register: React.FC = () => {
           query,
         });
         return;
-      } else {
-        throw new Error(res.description);
       }
     } catch (error: any) {
       const defaultLoginFailureMessage = '注册失败，请重试！';
-      message.error(error.message ?? defaultLoginFailureMessage);
+      message.error(defaultLoginFailureMessage);
     }
   };
   return (
